48dce80d06eedcc41af961ab68d8862602937bd5
Steve Brudz <sbrudz@defmethod.io>
2022-01-12 17:46:21 -0600

    Batch story transfers to avoid hitting rate limit errors

--- migrate.rb
+++ migrate.rb
@@ -11,17 +11,26 @@ shortcut_client = ShortcutRuby::Shortcut.new(ENV['SHORTCUT_API_TOKEN'])
 
 project = pivotal_client.project(ENV['PIVOTAL_PROJECT_ID'])
 members = MemberListFinder.new(shortcut_client: shortcut_client, pivotal_project: project).build
-stories = project.stories(filter: 'type:feature,chore,bug AND -state:accepted AND label:shortcut AND -label:migrated_to_shortcut')
 factory = ConverterFactory.new(members: members)
+
+stories = project.stories(filter: 'type:feature,chore,bug AND -state:accepted AND label:shortcut AND -label:migrated_to_shortcut')
 puts "Found #{stories.count} stories to transfer"
-formatted_stories = stories.map do |story|
-  factory.build_converter(story).convert
-end
-results = shortcut_client.stories.bulk_create(stories: formatted_stories)
-puts results
-if results[:code] == '201'
-  stories.each do |story|
+
+stories.each_slice(10) do |batch_of_stories|
+  print "Transferring stories #{batch_of_stories.map(&:id)}"
+  formatted_stories = batch_of_stories.map do |story|
+    factory.build_converter(story).convert
+  end
+
+  results = shortcut_client.stories.bulk_create(stories: formatted_stories)
+
+  raise "Error #{results[:code]} encountered" unless results[:code] == '201'
+
+  batch_of_stories.each do |story|
     story.add_label('migrated_to_shortcut')
     story.save
   end
+  puts '...Success!'
+  sleep 5
 end
+puts 'Transfer complete'
