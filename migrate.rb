760f9517b97c4dbd28e8d00396ac29f35683d3ab
Steve Brudz <sbrudz@defmethod.io>
2022-01-13 18:38:23 -0600

    Add support for dry run mode

--- migrate.rb
+++ migrate.rb
@@ -13,6 +13,9 @@ project = pivotal_client.project(ENV['PIVOTAL_PROJECT_ID'])
 members = MemberListFinder.new(shortcut_client: shortcut_client, pivotal_project: project).build
 factory = ConverterFactory.new(members: members)
 
+dry_run = ENV['DRY_RUN'] == 'true'
+puts '** Running in DRY_RUN mode - No updates will be made **' if dry_run
+
 stories = project.stories(filter: 'type:feature,chore,bug AND -state:accepted AND label:shortcut AND -label:migrated_to_shortcut')
 puts "Found #{stories.count} stories to transfer"
 
@@ -22,15 +25,19 @@ stories.each_slice(10) do |batch_of_stories|
     factory.build_converter(story).convert
   end
 
-  results = shortcut_client.stories.bulk_create(stories: formatted_stories)
+  if dry_run
+    puts '...Dry run complete'
+  else
+    results = shortcut_client.stories.bulk_create(stories: formatted_stories)
 
-  raise "Error #{results[:code]} encountered" unless results[:code] == '201'
+    raise "Error #{results[:code]} encountered" unless results[:code] == '201'
 
-  batch_of_stories.each do |story|
-    story.add_label('migrated_to_shortcut')
-    story.save
+    batch_of_stories.each do |story|
+      story.add_label('migrated_to_shortcut')
+      story.save
+    end
+    puts '...Success!'
   end
-  puts '...Success!'
   sleep 5
 end
-puts 'Transfer complete'
+puts "#{dry_run ? 'Dry run' : 'Transfer'} complete"
